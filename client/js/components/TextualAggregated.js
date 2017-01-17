340fac029531499aa88b2be42924c071e325688d
David Bruant <bruant.d@gmail.com>
2017-01-17 17:11:33 +0100

    Affichage des lignes M52 utilisées dans plus qu'une ligne d'aggrégation

--- client/js/components/TextualAggregated.js
+++ client/js/components/TextualAggregated.js
@@ -1,16 +1,38 @@
 import React from 'react'
 
-/*
-TODO :
-    * which M52 rows are unused (amount of money and % over M52 total)
-    * which M52 rows are used several times
-    * which agg rules gathered no M52 rows
- */
-
 function makeUnusedM52RowsSet(aggregatedInstruction, M52Instruction){
     return M52Instruction.filter(m52row => {
         return !aggregatedInstruction.some(aggRow => aggRow['M52Rows'].has(m52row))
     })
+}
+
+function makeUsedMoreThanOnceM52RowsSet(aggregatedInstruction, M52Instruction){
+    const m52RowToAggRows = new Map();
+
+    M52Instruction.forEach(m52row => {
+        const usingAggRows = new Set();
+        
+        aggregatedInstruction.forEach(aggRow => {
+            if(aggRow['M52Rows'].has(m52row)){
+                usingAggRows.add(aggRow);
+            }
+        });
+
+        if(usingAggRows.size >= 2){
+            m52RowToAggRows.set(m52row, usingAggRows)
+        }
+    });
+
+    return m52RowToAggRows;
+}
+
+function makeM52RowId(m52Row){
+    return [
+        m52Row['Dépense/Recette'] + m52Row['Investissement/Fonctionnement'],
+        m52Row["Chapitre"],
+        m52Row["Rubrique fonctionnelle"],
+        m52Row["Article"]
+    ].join(' ');
 }
 
 
@@ -19,8 +41,9 @@ export default function(props){
     const {aggregatedInstruction, M52Instruction} = props;
 
     const unusedM52Set = makeUnusedM52RowsSet(aggregatedInstruction, M52Instruction);
+    const usedMoreThanOnceM52RowsSet = makeUsedMoreThanOnceM52RowsSet(aggregatedInstruction, M52Instruction);
 
-    console.log('agg', aggregatedInstruction.toJS());
+    console.log('usedMoreThanOnceM52RowsSet', usedMoreThanOnceM52RowsSet);
 
     return React.createElement('div', {}, 
         React.createElement('table', {className: 'aggregated'}, aggregatedInstruction.map(aggRow => (
@@ -42,10 +65,22 @@ export default function(props){
         React.createElement('div', {}, 
             React.createElement('h1', {}, "Lignes M52 utilisées dans aucune formule d'aggrégation ("+unusedM52Set.size+")"),
             React.createElement('ul', {}, unusedM52Set.map(m52 => {
-                const m52Id = m52["Rubrique fonctionnelle"] + ' ' + m52["Article"] + ' ' + m52["Chapitre"];
+                const m52Id = makeM52RowId(m52);
 
                 return React.createElement('li', {key: m52Id}, m52Id)
             }))
-        )
+        ),
+        React.createElement('div', {}, 
+            React.createElement('h1', {}, "Lignes M52 utilisées dans au moins 2 formules d'aggrégation ("+usedMoreThanOnceM52RowsSet.size+")"),
+            React.createElement('ul', {}, Array.from(usedMoreThanOnceM52RowsSet).map(([m52Row, aggSet]) => {
+                const m52Id = makeM52RowId(m52Row);
+
+                return React.createElement('li', {key: m52Id}, 
+                    m52Id,
+                    ' utilisé dans ',
+                    [...aggSet].map(aggRow => aggRow.id).join(', ')
+                )
+            }))
+        )        
     );
 }
