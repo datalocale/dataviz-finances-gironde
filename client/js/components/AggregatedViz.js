421455a508d7d617c61f143b8639b0667d742e64
David Bruant <bruant.d@gmail.com>
2017-01-21 19:48:30 +0100

    Affichage d'un sunburst pour la vue agrégée

--- client/js/components/AggregatedViz.js
+++ client/js/components/AggregatedViz.js
@@ -1,6 +1,12 @@
 import React from 'react'
+<<<<<<< 8fa1805a8cf4efc5f8db4d384e5452f6cfccd6c9
 
 import Sunburst from './Sunburst';
+=======
+import Sunburst from './Sunburst';
+import TextualAggregated from './TextualAggregated';
+
+>>>>>>> Affichage d'un sunburst pour la vue agrégée
 import hierarchicalAggregated from '../finance/hierarchicalAggregated.js';
 
 /*
@@ -11,6 +17,7 @@ interface AggregatedViZProps{
 }
 
 */
+<<<<<<< 8fa1805a8cf4efc5f8db4d384e5452f6cfccd6c9
 export default function({aggregatedHierarchical, aggregatedSelectedNodes, onSliceSelected}){
     return React.createElement('div', {},
         React.createElement('h1', {}, aggregatedHierarchical.name),
@@ -19,5 +26,17 @@ export default function({aggregatedHierarchical, aggregatedSelectedNodes, onSlic
             selectedNodes: aggregatedSelectedNodes,
             onSliceSelected
         })
+=======
+export default function(props){
+    const {aggregatedInstruction, M52Instruction} = props;
+    const hierarchicalData = hierarchicalAggregated(aggregatedInstruction);
+
+    console.log('hierarchicalData', hierarchicalData);
+
+    return React.createElement('div', {},
+        // TODO : this shouldn't be just a sunburst but rather the choice of "par prestation" or "par public" should be offered
+        React.createElement(Sunburst, { hierarchicalData }),
+        React.createElement(TextualAggregated, props)
+>>>>>>> Affichage d'un sunburst pour la vue agrégée
     );
 }
--- client/js/components/M52Viz.js
+++ client/js/components/M52Viz.js
@@ -3,6 +3,7 @@ import Sunburst from './Sunburst';
 
 /*
 
+<<<<<<< 8fa1805a8cf4efc5f8db4d384e5452f6cfccd6c9
 interface M52VizProps{
     M52Hierarchical: M52Hierarchical
 }
@@ -10,6 +11,17 @@ interface M52VizProps{
  */
 
 export default function({ M52Hierarchical, M52SelectedNodes, onSliceSelected }){
+=======
+/*
+
+interface M52VizProps{
+    M52Instruction: M52Instruction
+}
+
+ */
+export default function(props){
+    const hierarchicalData = hierarchicalM52(props.M52Instruction);
+>>>>>>> Affichage d'un sunburst pour la vue agrégée
 
     return React.createElement('div', {},
         React.createElement('h1', {}, M52Hierarchical.name),
