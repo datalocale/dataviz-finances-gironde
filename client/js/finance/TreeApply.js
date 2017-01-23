3e2b3d265afc77fd04922d11c7c950f8012e597e
Thomas Parisot <hi@oncletom.io>
2017-01-23 11:11:40 +0100

    Travail préparatoire pour la gestion événementielle des actions utilisateur sur le Sunburst

---
+++ client/js/finance/TreeApply.js
@@ -0,0 +1,13 @@
+import { Set as ImmutableSet } from 'immutable';
+
+export default function TreeApply(tree, rowFn) {
+    const FlatMap = new ImmutableSet();
+
+    const walkTreeNode = (TreeNode) => {
+
+    };
+
+    walkTreeNode(tree);
+
+    return FlatMap;
+}
