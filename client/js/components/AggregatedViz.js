67a9c6193decadc7d6e2d844996eb38d99aa2a04
David Bruant <bruant.d@gmail.com>
2017-01-24 12:17:02 +0100

    Réparation de la hierarchie pour l'instruction aggrégée

--- client/js/components/AggregatedViz.js
+++ client/js/components/AggregatedViz.js
@@ -29,13 +29,13 @@ export default function({aggregatedHierarchical, aggregatedSelectedNodes, onSlic
 =======
 export default function(props){
     const {aggregatedInstruction, M52Instruction} = props;
-    //const hierarchicalData = hierarchicalAggregated(aggregatedInstruction);
+    const hierarchicalData = hierarchicalAggregated(aggregatedInstruction);
 
-    //console.log('hierarchicalData', hierarchicalData);
+    console.log('hierarchicalData', hierarchicalData);
 
     return React.createElement('div', {},
         // TODO : this shouldn't be just a sunburst but rather the choice of "par prestation" or "par public" should be offered
-        //React.createElement(Sunburst, { hierarchicalData }),
+        React.createElement(Sunburst, { hierarchicalData }),
         React.createElement(TextualAggregated, props)
 >>>>>>> Affichage d'un sunburst pour la vue agrégée
     );
