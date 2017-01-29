31711f3aab17df73f55d0ff4c3b0a10eb30e56a0
David Bruant <bruant.d@gmail.com>
2017-01-29 20:10:50 +0100

    Rajout du bundle + push sur DavidBruant/

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,10 +3,10 @@
 git config user.name "David Bruant (via Travis CI with personal access token)"
 git config user.email "bruant.d+travisci@gmail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
-git fetch upstream && git reset upstream/$DEPLOY_TARGET_BRANCH
+git remote add upstream "https://$GH_TOKEN@github.com/DavidBruant/dataviz-finances-gironde.git"
+git fetch origin $TRAVIS_BRANCH && git reset upstream/$DEPLOY_TARGET_BRANCH
 
 git add -A .
+git add -f client/js/browserify-bundle.js
 git commit -m "build of repo at commit https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_COMMIT"
 git push -q upstream HEAD:$DEPLOY_TARGET_BRANCH
-
