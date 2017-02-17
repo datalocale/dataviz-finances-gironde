bb6b7d429e6feb537f2d9f3d544155678b6c3070
David Bruant <bruant.d@gmail.com>
2017-02-17 15:19:13 +0100

    Adressage des commentaires d'@oncletom

--- tests/automated/m52ToAggregated.test.js
+++ tests/automated/m52ToAggregated.test.js
@@ -1,9 +1,9 @@
 import * as matchers from 'jest-immutable-matchers';
 import {OrderedSet as ImmutableSet} from 'immutable';
-import m52ToAggregated from '../../client/js/finance/m52ToAggregated';
+import {rules, default as m52ToAggregated} from '../../client/js/finance/m52ToAggregated';
 import {M52RowRecord, M52Instruction} from '../../client/js/finance/M52InstructionDataStructures';
 
-const AGGREGATED_ROWS_COUNT = 89;
+const AGGREGATED_ROWS_COUNT = Object.keys(rules).length;
 
 jest.addMatchers(matchers);
 
@@ -14,7 +14,8 @@ test('m52ToAggregated returns an OrderedSet', () => {
 
 test('m52ToAggregated returns an OrderedSet of one DF-3-1 element with same amount when passed an instruction with only DF C65 R12 A6553', () => {
   const AMOUNT = 1037;
-  const expectedAggId = 'DF-3-1';
+  const AGGREGATED_ROW_ID = 'DF-3-1';
+
   const m52Row = new M52RowRecord({
     'Dépense/Recette': 'D',
     'Investissement/Fonctionnement': 'F',
@@ -27,13 +28,13 @@ test('m52ToAggregated returns an OrderedSet of one DF-3-1 element with same amou
   const instruction = new M52Instruction({rows: new ImmutableSet([m52Row])});
 
   const aggVision = m52ToAggregated(instruction);
-  const aggDF22 = aggVision.filter(row => row.id === expectedAggId).first();
-  const otherAggRows = aggVision.filter(row => row.id !== expectedAggId);
+  const aggDF22 = aggVision.filter(row => row.id === AGGREGATED_ROW_ID).first();
+  const otherAggRows = aggVision.filter(row => row.id !== AGGREGATED_ROW_ID);
 
   expect(aggVision).toBeImmutableOrderedSet();
   expect(aggVision.size).toBe(AGGREGATED_ROWS_COUNT);
 
-  expect(aggDF22.id).toEqual(expectedAggId);
+  expect(aggDF22.id).toEqual(AGGREGATED_ROW_ID);
   expect(aggDF22.M52Rows).toBeImmutableSet();
   expect(aggDF22.M52Rows.first()).toBe(m52Row);
   expect(aggDF22["Montant"]).toBe(AMOUNT);
