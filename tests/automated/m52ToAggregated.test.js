1c1a9057bf3c13d0c112fa916f7b75b6f894e057
David Bruant <bruant.d@gmail.com>
2017-02-17 15:16:15 +0100

    Correction des tests suite au merge

--- tests/automated/m52ToAggregated.test.js
+++ tests/automated/m52ToAggregated.test.js
@@ -1,31 +1,32 @@
 import * as matchers from 'jest-immutable-matchers';
 import {OrderedSet as ImmutableSet} from 'immutable';
 import m52ToAggregated from '../../client/js/finance/m52ToAggregated';
-import M52RowRecord from '../../client/js/finance/M52RowRecord';
+import {M52RowRecord, M52Instruction} from '../../client/js/finance/M52InstructionDataStructures';
 
 const AGGREGATED_ROWS_COUNT = 89;
 
 jest.addMatchers(matchers);
 
 test('m52ToAggregated returns an OrderedSet', () => {
-  expect(m52ToAggregated(new ImmutableSet())).toBeImmutableOrderedSet();
+  const instruction = new M52Instruction({rows: new ImmutableSet()});
+  expect(m52ToAggregated(instruction)).toBeImmutableOrderedSet();
 });
 
-test('m52ToAggregated returns an OrderedSet of one DF-7-2 element with same amount when passed an instruction with only DF C011 R0202 A6156', () => {
+test('m52ToAggregated returns an OrderedSet of one DF-3-1 element with same amount when passed an instruction with only DF C65 R12 A6553', () => {
   const AMOUNT = 1037;
-  const expectedAggId = 'DF-7-2';
+  const expectedAggId = 'DF-3-1';
   const m52Row = new M52RowRecord({
     'Dépense/Recette': 'D',
     'Investissement/Fonctionnement': 'F',
     'Réel/Ordre id/Ordre diff': 'OR',
-    'Chapitre': 'C011',
-    'Article': 'A6156',
-    'Rubrique fonctionnelle': 'R0202',
+    'Chapitre': 'C65',
+    'Article': 'A6553',
+    'Rubrique fonctionnelle': 'R12',
     'Montant': AMOUNT
   })
-  const m52Instruction = ImmutableSet([m52Row]);
+  const instruction = new M52Instruction({rows: new ImmutableSet([m52Row])});
 
-  const aggVision = m52ToAggregated(m52Instruction);
+  const aggVision = m52ToAggregated(instruction);
   const aggDF22 = aggVision.filter(row => row.id === expectedAggId).first();
   const otherAggRows = aggVision.filter(row => row.id !== expectedAggId);
 
