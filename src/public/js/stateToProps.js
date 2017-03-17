cfaff6f8997f561bb9ef6398b2c8893bd7d68d17
David Bruant <bruant.d@gmail.com>
2017-03-17 13:33:42 +0100

    Début de partitionnement

--- src/public/js/stateToProps.js
+++ src/public/js/stateToProps.js
@@ -1,7 +1,12 @@
 import budgetBalance from '../../shared/js/finance/budgetBalance';
 
+
 export default function (state) {
     const { m52Instruction, breadcrumb, textsById } = state;
+
+    const displayedElement = breadcrumb.last();
+
+    
 
     return Object.assign(
         { breadcrumb, textsById },
