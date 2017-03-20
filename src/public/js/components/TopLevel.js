bf32ef74a18b52b8718d67f45508b6290a8b8ac0
David Bruant <bruant.d@gmail.com>
2017-03-20 13:16:40 +0100

    Suppression de la page FinanceExpert + nouveau squelette de navigation

--- src/public/js/components/TopLevel.js
+++ src/public/js/components/TopLevel.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import { HOME, FINANCE_EXPERT } from '../constants/pages';
+import { HOME } from '../constants/pages';
 
 import Breadcrumb from './Breadcrumb';
 import Home from './Home';
-import FinanceExpertPage from './FinanceExpertPage';
 import FinanceElement from './FinanceElement';
 
 export default function ({
@@ -20,16 +19,14 @@ export default function ({
         (breadcrumb.size >= 2 ? Breadcrumb({ breadcrumb, textsById, onContentChange }) : undefined),
         displayedElement === HOME ?
             React.createElement(Home, { breadcrumb, expenditures, revenue, onContentChange }) :
-            breadcrumb.size === 2 && displayedElement === FINANCE_EXPERT ?
-                React.createElement(FinanceExpertPage, {}) :
-                React.createElement(
-                    FinanceElement,
-                    {
-                        contentId: displayedElement,
-                        texts, total, partition,
-                        onGoDeeper: next => onContentChange(breadcrumb.push(next))
-                    }
-                )
+            React.createElement(
+                FinanceElement,
+                {
+                    contentId: displayedElement,
+                    texts, total, partition,
+                    onGoDeeper: next => onContentChange(breadcrumb.push(next))
+                }
+            )
     );
 }
 
