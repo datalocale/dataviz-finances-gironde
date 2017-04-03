8cee2abbc2793b119c50c0edc832ed9d7398c519
David Bruant <bruant.d@gmail.com>
2017-04-03 15:17:11 +0200

    Style du premier appetizer

--- src/public/js/components/TotalAppetizer.js
+++ src/public/js/components/TotalAppetizer.js
@@ -12,9 +12,11 @@ export default function ({total, year, totalUrl}) {
 
     return React.createElement('div', { className: 'appetizer total-appetizer' }, 
         React.createElement('h1', {}, 
-            React.createElement('span', {className: 'total'}, (total/Math.pow(10, 9)).toFixed(3).replace('.', ',')),
-            ' Milliards de dépenses en ',
-            year
+            React.createElement('div', {className: 'total'}, (total/Math.pow(10, 9)).toFixed(3).replace('.', ',')),
+            React.createElement('div', {}, 
+                ' Milliards de dépenses en ',
+                year
+            )
         ),
         React.createElement('hr', {}),
         React.createElement('p', {}, "Ce budget est composé de dépenses de fonctionnement, nécessaires aux missions et gestion des services de la collectivité, et de dépenses d’investissement dédiées à des programmes structurants ou stratégiques pour le territoire."),
