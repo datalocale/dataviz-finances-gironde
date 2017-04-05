b62edd03bc4b08ae905557b792509e36b95e5af7
David Bruant <bruant.d@gmail.com>
2017-04-05 22:00:30 +0200

    Premier jet du stackbar des solidarités

--- src/public/js/components/screens/FocusSolidarity.js
+++ src/public/js/components/screens/FocusSolidarity.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { Record } from 'immutable';
+import { scaleLinear } from 'd3-scale';
+import { min, max, sum } from 'd3-array';
+
+import budgetBalance from '../../../../shared/js/finance/budgetBalance';
+import m52ToAggregated from '../../../../shared/js/finance/m52ToAggregated';
+import hierarchicalAggregated from '../../../../shared/js/finance/hierarchicalAggregated';
+import {flattenTree} from '../../../../shared/js/finance/visitHierarchical';
+import {PAR_PUBLIC_VIEW, PAR_PRESTATION_VIEW} from '../../../../shared/js/finance/constants';
+
+
+
+function D3Axis({
+    tickData, className
+}){
+    return React.createElement('g', {className: ['d3-axis', className].filter(x => x).join(' ')}, 
+        tickData.map(({transform, line: {x1, y1, x2, y2}, text: {x, y, dx, dy, t} }) => {
+            return React.createElement('g', {className: 'tick', transform}, 
+                React.createElement('line', {x1, y1, x2, y2}),
+                React.createElement('text', {x, y, dx, dy}, t)
+            )
+        })
+    )
+}
+
+
+
 /*
 
 interface FocusSolidarityProps{
-    % de la dépense solidarité dans le budget
-
+    currentYear,
+    solidarityByYear: Map<year, Record<{
+        totalExpenditures,
+        solidarityExpenditures,
+        'DF-1-1',
+        'DF-1-2',
+        'DF-1-3',
+        'DF-1-4',
+        'DF-1-other',
+    }>[]
 }
 
 */
 
 
-export function FocusSol({}) {
+const WIDTH = 1000;
+const HEIGHT = 570;
+
+const HEIGHT_PADDING = 30;
+
+export function FocusSol({
+    currentYear, currentYearSolidarity, solidarityByYear
+}) {
+
+    const years = solidarityByYear.keySeq().toJS();
+
+    const columnAndMarginWidth = WIDTH/(years.length+1)
+    const columnMargin = columnAndMarginWidth/3;
+
+    const yearScale = scaleLinear()
+        .domain([min(years), max(years)])
+        .range([columnMargin, WIDTH-columnMargin]);
+
+    const solidarityTotals = solidarityByYear.valueSeq().toJS()
+        .map(ys => ys.solidarityExpenditures);
+
+    const maxSolidarityTotal = max(solidarityTotals);
+
+
+
+    const yAxisAmountScale = scaleLinear()
+        .domain([0, maxSolidarityTotal])
+        .range([HEIGHT - HEIGHT_PADDING, HEIGHT_PADDING]);
+    const yRange = yAxisAmountScale.range()[0] - yAxisAmountScale.range()[1];
+
+    const ticks = yAxisAmountScale.ticks(5);
+
+    const rectAmountScale = scaleLinear()
+        .domain([0, maxSolidarityTotal])
+        .range([0, yRange]);
+
 
     return React.createElement('article', {className: 'focus'},
         React.createElement('section', {}, 
@@ -35,7 +105,75 @@ export function FocusSol({}) {
         ),
         React.createElement('section', {}, 
             React.createElement('h2', {}, `Evolution des dépenses de “Solidarités” par prestation de XXX à YYY`),
-            React.createElement('stacked-bar', {})
+            React.createElement('div', {className: 'solidarity-by-year'},
+                React.createElement('svg', {width: WIDTH, height: HEIGHT},
+                    // x axis / years
+                    React.createElement(D3Axis, {className: 'x', tickData: 
+                        years.map(y => {
+                            return {
+                                transform: `translate(${yearScale(y)}, ${HEIGHT-HEIGHT_PADDING})`,
+                                line: { x1 : 0, y1 : 0, x2 : 0, y2 : 0 }, 
+                                text: {
+                                    x: 0, y: -10, 
+                                    dx: "-1.6em", dy: 0, 
+                                    t: y
+                                }
+                                
+                            }
+                        })
+                    }),
+                    // y axis / money amounts
+                    React.createElement(D3Axis, {className: 'y', tickData: ticks.map(tick => {
+                        return {
+                            transform: `translate(0, ${yAxisAmountScale(tick)})`,
+                            line: {
+                                x1 : 0, y1 : 0, 
+                                x2 : WIDTH, y2 : 0
+                            }, 
+                            text: {
+                                x: 0, y: -10, 
+                                dx: 0, dy: 0, 
+                                t: (tick/1000000)+'M'
+                            }
+                            
+                        }
+                    })}),
+                    // content
+                    React.createElement('g', {className: 'content'},
+                        solidarityByYear.entrySeq().toJS().map(([year, yearSolidarity], i) => {
+                            const stackElements = ['DF-1-1', 'DF-1-2', 'DF-1-3', 'DF-1-4', 'DF-1-other'];
+                            const stackYs = stackElements
+                                .map(id => yearSolidarity[id])
+                                .map( (amount, i, arr) => sum(arr.slice(0, i)) )
+                                .map(rectAmountScale);
+
+                            const stack = stackElements
+                                .map((id, i) => {
+                                    const amount = yearSolidarity[id];
+                                    const height = rectAmountScale(amount);
+
+                                    return {
+                                        amount,
+                                        height,
+                                        y: HEIGHT - HEIGHT_PADDING - height - stackYs[i]
+                                    }
+                                }) 
+
+
+                            return React.createElement('g', {className: 'column', transform: `translate(${yearScale(year)})`}, 
+                                React.createElement('text', {}, yearSolidarity.solidarityExpenditures),
+                                stack.map( ({amount, height, y}) => {
+                                    return React.createElement('g', {}, 
+                                        React.createElement('rect', {x: 0, y, width: 100, height}),
+                                        React.createElement('text', {x: 0, y, width: 100, height}, (amount/1000000).toFixed(1))
+                                    )
+                                })
+                            )
+
+                        })
+                    )
+                )
+            )
         ),
         React.createElement('section', {}, 
             React.createElement('h2', {}, `Les publics`),
@@ -49,10 +187,53 @@ export function FocusSol({}) {
 
 }
 
+const YearSolidarityRecord = Record({
+    totalExpenditures: 0,
+    solidarityExpenditures: 0,
+    'DF-1-1': 0,
+    'DF-1-2': 0,
+    'DF-1-3': 0,
+    'DF-1-4': 0,
+    'DF-1-other': 0
+})
+
 export default connect(
     state => {
-        const { breadcrumb } = state;
-        return {focusId: breadcrumb.last()};
+        const { m52InstructionByYear, currentYear } = state;
+
+        const solidarityByYear = m52InstructionByYear.map( (instruction => {
+            const {expenditures} = budgetBalance(instruction);
+            const agg = m52ToAggregated(instruction);
+            //const hierAggByPublic = hierarchicalAggregated(agg, {rd: 'D', fi: 'F'}, PAR_PUBLIC_VIEW);
+            const hierAggByPrestation = hierarchicalAggregated(agg, {rd: 'D', fi: 'F'}, PAR_PRESTATION_VIEW);
+
+            const hierAggByPrestationList = flattenTree(hierAggByPrestation);
+            console.log('hierAggByPrestationList', hierAggByPrestationList);
+
+            const solidarityExpenditures = hierAggByPrestationList.find(e => e.id === 'DF-1').total;
+            const df11 = hierAggByPrestationList.find(e => e.id === 'DF-1-1').total;
+            const df12 = hierAggByPrestationList.find(e => e.id === 'DF-1-2').total;
+            const df13 = hierAggByPrestationList.find(e => e.id === 'DF-1-3').total;
+            const df14 = hierAggByPrestationList.find(e => e.id === 'DF-1-4').total;
+            const df1other = solidarityExpenditures - (df11 + df12 + df13 + df14);
+
+            return new YearSolidarityRecord({
+                totalExpenditures: expenditures,
+                solidarityExpenditures,
+                'DF-1-1': df11,
+                'DF-1-2': df12,
+                'DF-1-3': df13,
+                'DF-1-4': df14,
+                'DF-1-other': df1other
+            })
+        }) )
+
+
+        return {
+            currentYear,
+            currentYearSolidarity: solidarityByYear.get(currentYear),
+            solidarityByYear
+        };
     },
     () => ({})
 )(FocusSol);
