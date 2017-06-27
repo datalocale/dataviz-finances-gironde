74b666c9dc41c509aaa637f9e92459b205f108c2
David Bruant <bruant.d@gmail.com>
2017-06-27 14:57:19 +0200

    Style de l'appetizer principal

--- src/public/js/components/TotalAppetizer.js
+++ src/public/js/components/TotalAppetizer.js
@@ -14,16 +14,17 @@ export default function ({total, year, exploreUrl}) {
 
     return React.createElement('div', { className: 'appetizer total-appetizer' }, 
         React.createElement('h1', {}, 
-            React.createElement('div', {className: 'total'}, 
-                total ? (total/Math.pow(10, 9)).toFixed(3).replace('.', ',') : ''
+            React.createElement('div', {className: 'number'}, 
+                total ? (total/Math.pow(10, 9)).toFixed(1).replace('.', ',') : ''
             ),
-            React.createElement('div', {}, 
-                ' Milliards de dépenses en ',
-                year
+            React.createElement('div', {className: 'text'}, 
+                `Milliards de dépenses en ${year}`
             )
         ),
         React.createElement('hr', {}),
-        React.createElement('p', {}, `Ce budget est composé de dépenses de fonctionnement, nécessaires aux missions et gestion des services de la collectivité, et de dépenses d’investissement dédiées à des programmes structurants ou stratégiques pour le territoire.`),
+        React.createElement('p', {}, 
+            `Ce budget est composé de dépenses de fonctionnement, nécessaires aux missions et gestion des services de la collectivité, et de dépenses d’investissement dédiées à des programmes structurants ou stratégiques pour le territoire.`
+        ),
         React.createElement(PrimaryCallToAction, { href: exploreUrl, text: 'Explorer le budget'})
     );
 }
