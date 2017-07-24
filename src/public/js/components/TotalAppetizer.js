5176dd2347259c8f373cde2fee56e46d2f3812e9
David Bruant <bruant.d@gmail.com>
2017-07-24 18:25:24 +0200

    Paramétrisation du TotalAppetizer

--- src/public/js/components/TotalAppetizer.js
+++ src/public/js/components/TotalAppetizer.js
@@ -12,18 +12,18 @@ interface TotalAppetizerProps{
 
 export default function ({total, year, exploreUrl}) {
 
+    const amount = total ? (total/Math.pow(10, 9)).toFixed(1).replace('.', ',') : '';
+
     return React.createElement('div', { className: 'appetizer total-appetizer' }, 
         React.createElement('h1', {}, 
-            React.createElement('div', {className: 'number'}, 
-                total ? (total/Math.pow(10, 9)).toFixed(1).replace('.', ',') : ''
-            ),
+            React.createElement('div', {className: 'number'}, amount),
             React.createElement('div', {className: 'text'}, 
                 `Milliards de dépenses en ${year}`
             )
         ),
         React.createElement('hr', {}),
         React.createElement('p', {}, 
-            `Le département a dépensé 1,6 milliards d’euros pour les girondins en 2016. Explorez les comptes pour comprendre d’où vient cet argent, à quoi il sert et comment il a été dépensé.`
+            `Le département a dépensé ${amount} milliards d’euros pour les girondins en ${year}. Explorez les comptes pour comprendre d’où vient cet argent, à quoi il sert et comment il a été dépensé.`
         ),
         React.createElement(PrimaryCallToAction, { href: exploreUrl, text: 'Explorer le budget'})
     );
