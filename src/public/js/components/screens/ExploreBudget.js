4234933bc118bacc14b5dfef21471a55728300df
Thomas Parisot <hi@oncletom.io>
2017-09-19 21:18:07 +0200

    feature(content): transform plain text paragraphs into Markdown rendered HTML

--- src/public/js/components/screens/ExploreBudget.js
+++ src/public/js/components/screens/ExploreBudget.js
@@ -16,6 +16,7 @@ import PageTitle from '../../../../shared/js/components/gironde.fr/PageTitle';
 import SecundaryTitle from '../../../../shared/js/components/gironde.fr/SecundaryTitle';
 import DownloadSection from '../../../../shared/js/components/gironde.fr/DownloadSection';
 import PrimaryCallToAction from '../../../../shared/js/components/gironde.fr/PrimaryCallToAction';
+import Markdown from '../../../../shared/js/components/Markdown';
 
 import BudgetConstructionAnimation from '../BudgetConstructionAnimation'
 
@@ -27,16 +28,16 @@ function displayMillions(amount){
 }
 
 export function TotalBudget({
-    currentYear, totalById, m52Instruction, labelsById, 
-    urls: {expenditures: expURL, revenue: revURL, rf, ri, df, di, byFonction}, 
+    currentYear, totalById, m52Instruction, labelsById,
+    urls: {expenditures: expURL, revenue: revURL, rf, ri, df, di, byFonction},
     constructionAmounts}) {
     const expenditures = totalById.get(EXPENDITURES)
     const revenue = totalById.get(REVENUE)
 
     const max = Math.max(expenditures, revenue);
 
-    const expHeight = MAX_HEIGHT*(expenditures/max)+'em'; 
-    const revHeight = MAX_HEIGHT*(revenue/max)+'em'; 
+    const expHeight = MAX_HEIGHT*(expenditures/max)+'em';
+    const revHeight = MAX_HEIGHT*(revenue/max)+'em';
 
     const rfHeight = 100*(totalById.get(RF)/revenue)+'%';
     const riHeight = 100*(totalById.get(RI)/revenue)+'%';
@@ -45,24 +46,24 @@ export function TotalBudget({
 
     return React.createElement('article', {className: 'explore-budget'},
         React.createElement(PageTitle, {text: `Exploration des comptes ${currentYear}`}),
-        React.createElement('section', {}, 
-            React.createElement('p', {},
+        React.createElement('section', {},
+            React.createElement(Markdown, {},
                 `L'exécution du budget 2016, premier de la mandature du président Jean-Luc Gleyze, a été marqué par l’augmentation de la contribution des collectivités locales à la réduction des déficits publics et aux évolution du périmètre d’intervention du département suite au vote des lois MAPTAM et NOTRe. Le Département de la Gironde s’est adapté en resserrant ses marges d’autofinancement et a travaillé sur la maîtrise des dépenses de fonctionnement. Cette rigueur a permis de préserver les dépenses sociales, obligatoires et incompressibles tout en conservant les dépenses d’investissement.`
             ),
-            React.createElement('p', {},
+            React.createElement(Markdown, {},
                 `Ainsi les résultats financiers de la Gironde pour cet exercice se traduisent par :
                 une l’épargne brute en nette amélioration, fruit notamment d’une gestion rigoureuse des dépenses de fonctionnement
                 une  réduction du besoin de financement par emprunt qui entraîne une baisse du ratio de financement en % des recettes de fonctionnement indicateur de la performance financière`
             )
         ),
- 
+
         React.createElement('section', {},
             React.createElement(SecundaryTitle, {text: 'Les grandes masses budgétaires'}),
             React.createElement('div', {className: 'viz'},
                 React.createElement('div', {className: 'revenue'},
                     React.createElement('h1', {}, 'Recettes'),
-                    React.createElement('div', {}, 
-                        React.createElement('div', {className: 'areas', style: {height: revHeight}}, 
+                    React.createElement('div', {},
+                        React.createElement('div', {className: 'areas', style: {height: revHeight}},
                             React.createElement('a', {className: 'rf', href: rf, style: {height: rfHeight}},
                                 React.createElement('h2', {}, "Recettes de fonctionnement"),
                                 React.createElement('h3', {}, displayMillions(totalById.get(RF)))
@@ -72,8 +73,8 @@ export function TotalBudget({
                                 React.createElement('h3', {}, displayMillions(totalById.get(RI)))
                             )
                         ),
-                        React.createElement('div', {className: 'texts'}, 
-                            React.createElement('div', {}, 
+                        React.createElement('div', {className: 'texts'},
+                            React.createElement('div', {},
                                 React.createElement('div', {className: 'amount'}, (revenue/Math.pow(10, 9)).toFixed(2).replace('.', ',')),
                                 React.createElement('div', {className: 'unit'}, `milliards d'euros`)
                             ),
@@ -83,8 +84,8 @@ export function TotalBudget({
                 ),
                 React.createElement('div', {className: 'expenditures'},
                     React.createElement('h1', {}, 'Dépenses'),
-                    React.createElement('div', {}, 
-                        React.createElement('div', {className: 'areas', style: {height: expHeight}}, 
+                    React.createElement('div', {},
+                        React.createElement('div', {className: 'areas', style: {height: expHeight}},
                             React.createElement('a', {className: 'df', href: df, style: {height: dfHeight}},
                                 React.createElement('h2', {}, "Dépenses de fonctionnement"),
                                 React.createElement('h3', {}, displayMillions(totalById.get(DF)))
@@ -95,7 +96,7 @@ export function TotalBudget({
                             )
                         ),
                         React.createElement('div', {className: 'texts'},
-                            React.createElement('div', {}, 
+                            React.createElement('div', {},
                                 React.createElement('div', {className: 'amount'}, (expenditures/Math.pow(10, 9)).toFixed(2).replace('.', ',')),
                                 React.createElement('div', {className: 'unit'}, `milliards d'euros`)
                             ),
@@ -108,12 +109,12 @@ export function TotalBudget({
         React.createElement('section', {},
             React.createElement(SecundaryTitle, {text: `Comprendre la construction d'un budget`}),
             React.createElement(
-                'p',
+                Markdown,
                 {},
                 `Le budget prévoit la répartition des recettes et des dépenses sur un exercice. Il est composé de la section de fonctionnement et d’investissement. Contrairement à l’Etat, les Départements, ont l’obligation d’adopter un budget à l’équilibre. Cela signifie que les Départements ne peuvent pas présenter de déficit.`
             ),
             React.createElement(
-                'p',
+                Markdown,
                 {},
                 `Dans un contexte particulièrement contraint, la préservation de nos équilibres financiers constitue un défi stimulant. Alors comment s’établit notre budget ?`
             ),
@@ -122,11 +123,11 @@ export function TotalBudget({
                 constructionAmounts
             )
         ),
-        React.createElement('section', {className: 'm52'}, 
+        React.createElement('section', {className: 'm52'},
             React.createElement(SecundaryTitle, {text: 'Les comptes sous la norme M52'}),
             m52Instruction ? React.createElement(M52ByFonction, {m52Instruction, urlByFonction: byFonction, labelsById}) : undefined,
             React.createElement(
-                DownloadSection, 
+                DownloadSection,
                 {
                     title: `Données brutes sur datalocale.fr`,
                     items: [
@@ -170,22 +171,22 @@ export default connect(
                 Solidarité: totalById.get('DF-1'),
                 Interventions: totalById.get('DF-3') + totalById.get('DF-4'),
                 DépensesStructure: (totalById.get('DF') - sum(['DF-1', 'DF-3', 'DF-4'].map(i => totalById.get(i)))),
-                
-                Emprunt: totalById.get('RI-EM'),
-                RIPropre: (totalById.get('RI') - totalById.get('RI-EM')), 
 
-                RemboursementEmprunt: totalById.get('DI-EM'), 
-                Routes: totalById.get('DI-1-2'), 
-                Colleges: totalById.get('DI-1-1'), 
-                Amenagement: totalById.get('DI-1-3') + totalById.get('DI-1-4') + totalById.get('DI-1-5'), 
+                Emprunt: totalById.get('RI-EM'),
+                RIPropre: (totalById.get('RI') - totalById.get('RI-EM')),
+
+                RemboursementEmprunt: totalById.get('DI-EM'),
+                Routes: totalById.get('DI-1-2'),
+                Colleges: totalById.get('DI-1-1'),
+                Amenagement: totalById.get('DI-1-3') + totalById.get('DI-1-4') + totalById.get('DI-1-5'),
                 Subventions: totalById.get('DI-2')
             } : undefined,
             urls: {
-                expenditures: '#!/finance-details/'+EXPENDITURES, 
-                revenue: '#!/finance-details/'+REVENUE, 
-                rf: '#!/finance-details/'+RF, 
-                ri: '#!/finance-details/'+RI, 
-                df: '#!/finance-details/'+DF, 
+                expenditures: '#!/finance-details/'+EXPENDITURES,
+                revenue: '#!/finance-details/'+REVENUE,
+                rf: '#!/finance-details/'+RF,
+                ri: '#!/finance-details/'+RI,
+                df: '#!/finance-details/'+DF,
                 di: '#!/finance-details/'+DI,
                 byFonction: {
                     'M52-DF-R0': `#!/finance-details/M52-DF-R0`,
--- src/public/js/components/screens/FocusSolidarity.js
+++ src/public/js/components/screens/FocusSolidarity.js
@@ -11,6 +11,7 @@ import StackChart from '../../../../shared/js/components/StackChart';
 import PageTitle from '../../../../shared/js/components/gironde.fr/PageTitle';
 import SecundaryTitle from '../../../../shared/js/components/gironde.fr/SecundaryTitle';
 import PrimaryCallToAction from '../../../../shared/js/components/gironde.fr/PrimaryCallToAction';
+import Markdown from '../../../../shared/js/components/Markdown';
 
 import FocusDetail from '../FocusDetail';
 import FocusDonut from '../FocusDonut';
@@ -39,38 +40,35 @@ export function FocusSol({
 
 
     return React.createElement('article', {className: 'focus'},
-        React.createElement('section', {}, 
+        React.createElement('section', {},
             React.createElement(PageTitle, {text: 'Solidarités'}),
-            React.createElement('p', {}, 
+            React.createElement(Markdown, {},
                 `Face à la croissance des situations d’exclusion et de précarité, le Département poursuit ses actions sociales innovantes et s’affirme en particulier dans le domaine de l’insertion et de l’accompagnement des personnes en difficultés. Allocations, prestations sociales et solidarité : de nombreux girondins bénéficient d’une ou plusieurs aides dans leurs parcours de vie au quotidien.`
             )
         ),
-        React.createElement('section', {className: 'top-infos'}, 
+        React.createElement('section', {className: 'top-infos'},
             React.createElement(FocusDonut, {
-                proportion: solidarityProportion, 
-                outerRadius: 188, 
+                proportion: solidarityProportion,
+                outerRadius: 188,
                 innerText: [
                     `de dépenses Solidarités`,
                     `dans le total des dépenses`
                 ]
             }),
-            React.createElement('div', {}, 
-                React.createElement('p', {}, 
-                    React.createElement('strong', {},
-                        "Avec 120 000 prestations allouées et xxx millions d'euros mobilisés en 2016, les dépenses de Solidarités pour soutenir les personnes fragilisées évoluent de +4,31% par rapport à 2015."
-                    ),
-                    ` `),
+            React.createElement('div', {},
+                React.createElement(Markdown, {},
+                    "**Avec 120 000 prestations allouées et xxx millions d'euros mobilisés en 2016, les dépenses de Solidarités pour soutenir les personnes fragilisées évoluent de +4,31% par rapport à 2015**."),
                 React.createElement(PrimaryCallToAction, {href: '#!/finance-details/DF-2', text: `en savoir plus`})
             ),
-            React.createElement('div', {className: 'people-fraction'}, 
-                React.createElement('div', {}, 
+            React.createElement('div', {className: 'people-fraction'},
+                React.createElement('div', {},
                     React.createElement('div', {}, 'Près de'),
                     React.createElement('div', {className: 'number'}, '1/10'),
                     React.createElement('div', {}, `personne accompagnée par le département`)
                 )
             )
         ),
-        React.createElement('section', {}, 
+        React.createElement('section', {},
             React.createElement(SecundaryTitle, {text: `Les dépenses "Solidarités" augmentent pour tous les publics`}),
             React.createElement(StackChart, {
                 xs: years,
@@ -85,45 +83,45 @@ export function FocusSol({
                 legendItems: [
                     {
                         id: 'DF-2-1',
-                        colorClassName: 'DF-2-1', 
+                        colorClassName: 'DF-2-1',
                         text: "Personnes en insertion"
                     },
                     {
                         id: 'DF-2-2',
-                        colorClassName: 'DF-2-2', 
+                        colorClassName: 'DF-2-2',
                         text: "Personnes handicapées"
                     },
                     {
                         id: 'DF-2-3',
-                        colorClassName: 'DF-2-3', 
+                        colorClassName: 'DF-2-3',
                         text: "Personnes âgées"
                     },
                     {
                         id: 'DF-2-4',
-                        colorClassName: 'DF-2-4', 
+                        colorClassName: 'DF-2-4',
                         text: "Enfance"
                     },
                     {
                         id: 'DF-2-other',
-                        colorClassName: 'DF-2-other', 
+                        colorClassName: 'DF-2-other',
                         text: "Prévention transversale"
                     }
                 ]
             })
         ),
-        React.createElement('section', {}, 
+        React.createElement('section', {},
             React.createElement(SecundaryTitle, {text: `Les actions et les aides varient en fonction des publics`}),
-            React.createElement('p', {}, `Les dépenses de solidarité concernent quatre catégories de bénéficiaires : les personnes en insertion ou en situation de précarité, les personnes handicapée, les personnes âgées, les enfants. 
+            React.createElement(Markdown, {}, `Les dépenses de solidarité concernent quatre catégories de bénéficiaires : les personnes en insertion ou en situation de précarité, les personnes handicapée, les personnes âgées, les enfants.
 Le Département définit sa propre politique et les actions qu’il met en œuvre pour chacun de ces publics : hébergements, prestations, subventions, allocations.`),
             React.createElement(FocusDetail, {
-                className: 'insertion', 
-                title: 'Personnes en insertion', 
-                illustrationUrl: '../images/Macaron1.png', 
+                className: 'insertion',
+                title: 'Personnes en insertion',
+                illustrationUrl: '../images/Macaron1.png',
                 // (May 29th) different than what was hardcoded ("244 Millions €")
-                amount: currentYearSolidarity ? format(".3s")(currentYearSolidarity.get('DF-2-1')) : '', 
-                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-1')/currentYearSolidarity.solidarityExpenditures : 1, 
+                amount: currentYearSolidarity ? format(".3s")(currentYearSolidarity.get('DF-2-1')) : '',
+                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-1')/currentYearSolidarity.solidarityExpenditures : 1,
                 text: `A lui seul, le RSA (Revenu de Solidarité Active) représente presque 94% des aides allouées aux personnes en insertion. Une personne française ou étrangère d'au moins 25 ans peut en bénéficier si elle remplit plusieurs conditions. https://www.service-public.fr/particuliers/vosdroits/F19778
-       `, 
+       `,
                 highlights: [
                     {
                         strong: "+32%",
@@ -137,21 +135,21 @@ Le Département définit sa propre politique et les actions qu’il met en œuvr
                         strong: "+5.5%",
                         span: "d'allocations RSA par rapport à 2015"
                     }
-                ], 
+                ],
                 moreUrl: '#!/finance-details/DF-2-1'
             }),
             React.createElement(FocusDetail, {
-                className: 'handicap', 
-                title: 'Personnes handicapées', 
+                className: 'handicap',
+                title: 'Personnes handicapées',
                 illustrationUrl: '../images/Macaron2.png',
                 // (May 29th) different than what was hardcoded ("218 Millions €",)
-                amount: currentYearSolidarity ? format(".3s")(currentYearSolidarity.get('DF-2-2')) : '', 
-                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-2')/currentYearSolidarity.solidarityExpenditures : 1, 
+                amount: currentYearSolidarity ? format(".3s")(currentYearSolidarity.get('DF-2-2')) : '',
+                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-2')/currentYearSolidarity.solidarityExpenditures : 1,
                 text: `Trois types d’aides ont été allouées aux personnes handicapées par le Département en 2016 :
-- La PCH (Prestation de Compensation du Handicap), a été versée à 9 975 personnes bénéficiaires 
+- La PCH (Prestation de Compensation du Handicap), a été versée à 9 975 personnes bénéficiaires
 - La prestation d’hébergement a financé 2763 places d'hébergement
-- L'ACTP (Allocation de Compensation pour Tiers Personne) a financé l'emploi d'aides à domicile pour 1 128 personnes 
-(All`, 
+- L'ACTP (Allocation de Compensation pour Tiers Personne) a financé l'emploi d'aides à domicile pour 1 128 personnes
+(All`,
                 highlights: [
                     {
                         strong: "73 Millions d'euros",
@@ -165,15 +163,15 @@ Le Département définit sa propre politique et les actions qu’il met en œuvr
                         strong: "8.25 Millions d'euros",
                         span: "pour l'emploi de 757 aides à domicile"
                     }
-                ], 
+                ],
                 moreUrl: '#!/finance-details/DF-2-2'
             }),
             React.createElement(FocusDetail, {
-                className: 'elderly', 
-                title: 'Personnes âgées', 
+                className: 'elderly',
+                title: 'Personnes âgées',
                 illustrationUrl: '../images/Macaron3.png',
                 amount: currentYearSolidarity ? format(".3s")(currentYearSolidarity.get('DF-2-3')) : '',
-                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-3')/currentYearSolidarity.solidarityExpenditures : 1, 
+                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-3')/currentYearSolidarity.solidarityExpenditures : 1,
                 text: `La principale aide en faveur des personnes âgées est l’APA (Allocation Personnalisée d’autonomie). Elle peut être versée soit directement à la personne soit à l’établissement en charge de la personne, selon des critères d'attribution précis. https://www.service-public.fr/particuliers/vosdroits/F10009 L’année 2016 est marquée par l’augmentation des versements de l’APA liée à la mise en place de la loi ASV (Adaptation de la Société au Vieillissement).`,
                 highlights: [
                     {
@@ -188,17 +186,17 @@ Le Département définit sa propre politique et les actions qu’il met en œuvr
                         strong: "34 046 ",
                         span: "bénéficiaires en 2016"
                     }
-                ], 
+                ],
                 moreUrl: '#!/finance-details/DF-2-3'
             }),
             React.createElement(FocusDetail, {
-                className: 'childhood', 
-                title: 'Enfance', 
+                className: 'childhood',
+                title: 'Enfance',
                 illustrationUrl: '../images/Macaron4.png',
                 // (May 29th) different than what was hardcoded ("168 Millions €")
                 amount: currentYearSolidarity ? format(".3s")(currentYearSolidarity.get('DF-2-4')) : '',
-                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-4')/currentYearSolidarity.solidarityExpenditures : 1, 
-                text: `L’Aide Sociale à l'Enfance (ASE) est le service du Département responsable de la protection des mineurs en danger ou en risque de danger (loi du 5 mars 2007). Les missions de l’ASE : apporter un soutien aux familles à leur domicile et accueillir les enfants qui lui sont confiés par leurs parents ou par un juge. Le Département s'occupe également de l’accueil familial qui représente le deuxième mode d’accueil des mineurs.`, 
+                proportion: currentYearSolidarity ? currentYearSolidarity.get('DF-2-4')/currentYearSolidarity.solidarityExpenditures : 1,
+                text: `L’Aide Sociale à l'Enfance (ASE) est le service du Département responsable de la protection des mineurs en danger ou en risque de danger (loi du 5 mars 2007). Les missions de l’ASE : apporter un soutien aux familles à leur domicile et accueillir les enfants qui lui sont confiés par leurs parents ou par un juge. Le Département s'occupe également de l’accueil familial qui représente le deuxième mode d’accueil des mineurs.`,
                 highlights: [
                     {
                         strong: "166 M d'€",
@@ -212,7 +210,7 @@ Le Département définit sa propre politique et les actions qu’il met en œuvr
                         strong: "835",
                         span: " assistants familiaux pour l'accueil familial"
                     }
-                ], 
+                ],
                 moreUrl: '#!/finance-details/DF-2-4'
             })
         )
