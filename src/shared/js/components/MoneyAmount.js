e0bbbfcc98b333550659e73b1e1a50e579304911
David Bruant <bruant.d@gmail.com>
2017-09-24 15:06:10 +0200

    Composant pour les montants d'argent

---
+++ src/shared/js/components/MoneyAmount.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MILLION = 1000000;
+
+export function makeAmountString(amount){
+    return (
+        amount < 100000 ?
+    `${(amount).toFixed(0)}€` :
+    `${(amount/MILLION).toFixed(1)}M d'€`
+    )
+    .replace('.', ',');
+}
+
+export default ({amount}) => {
+    return React.createElement('span', {className: 'money-amount'}, makeAmountString(amount));
+};
