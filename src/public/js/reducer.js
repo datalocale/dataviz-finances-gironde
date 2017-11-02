76ea66d61c67fa1fe18682398597d81e099449ba
David Bruant <bruant.d@gmail.com>
2017-11-02 12:32:37 +0100

    Compute all finance data trees as soon as data arrives instead of just-in-time. Fixes #291

--- src/public/js/reducer.js
+++ src/public/js/reducer.js
@@ -1,11 +1,13 @@
 import { Record } from 'immutable';
 import { markdown as md } from '../../shared/js/components/Markdown';
+import { hierarchicalM52, hierarchicalAggregated, m52ToAggregated } from '../../shared/js/finance/memoized';
 
 import {
     FINANCE_DETAIL_ID_CHANGE, M52_INSTRUCTION_RECEIVED, CORRECTION_AGGREGATION_RECEIVED,
     ATEMPORAL_TEXTS_RECEIVED, TEMPORAL_TEXTS_RECEIVED,
     CHANGE_EXPLORATION_YEAR
 } from './constants/actions';
+import { DF, DI } from './constants/pages';
 
 const FinanceElementTextsRecord = Record({
     label: undefined,
@@ -19,7 +21,21 @@ export default function reducer(state, action) {
 
     switch (type) {
         case M52_INSTRUCTION_RECEIVED:{
-            const {m52Instruction} = action
+            const {m52Instruction} = action;
+            const {corrections} = state;
+
+            if(corrections){
+                // these variables will be unused. These calls exist for the sole purpose of memoization as
+                // soon as the data arrives
+                const aggregated = m52ToAggregated(m52Instruction, corrections);
+                const hierAgg = hierarchicalAggregated(aggregated);
+                const hierarchicalM52DF = hierarchicalM52(m52Instruction, DF);
+                const hierarchicalM52DI = hierarchicalM52(m52Instruction, DI);
+                
+                // to prevent minifyer optimizations
+                console.log('memz', hierarchicalM52DI, hierarchicalM52DF, hierAgg);
+            }
+
             return state.setIn(['m52InstructionByYear', m52Instruction.year], m52Instruction);
         }
         case CORRECTION_AGGREGATION_RECEIVED: {
